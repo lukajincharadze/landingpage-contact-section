b3520ef13946be3b9456ee01b87f31ead84562fb
Luka Jincharadze <lukajinchara@gmail.com>
2023-04-27 18:59:31 +0400

    update
    added some transitions to look better

--- js/javascript.js
+++ js/javascript.js
@@ -2,11 +2,16 @@ let navbarLinks = document.getElementById("navbarLinks");
 let togglebutton = document.getElementById("toggle-button");
 let navigation = document.getElementById("navigation");
 
+navigation.style.transition = "height 0.3s ease-in-out";
+navbarLinks.style.transition = "opacity 0.3s ease-in-out";
+
 togglebutton.addEventListener("click", function() {
     navbarLinks.classList.toggle("active");
     if (navigation.style.height === "324px") {
         navigation.style.height = "250px";
+        navbarLinks.style.opacity = "0";
     } else {
         navigation.style.height = "324px";
+        navbarLinks.style.opacity = "1";
     }
 });
