31b4d4de39b37119e5c7d94fd2a8206b6174518a
Luka Jincharadze <lukajinchara@gmail.com>
2023-04-27 18:34:31 +0400

    done till 850px

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,12 @@
 let navbarLinks = document.getElementById("navbarLinks");
 let togglebutton = document.getElementById("toggle-button");
+let navigation = document.getElementById("navigation");
 
 togglebutton.addEventListener("click", function() {
     navbarLinks.classList.toggle("active");
-})
+    if (navigation.style.height === "324px") {
+        navigation.style.height = "250px";
+    } else {
+        navigation.style.height = "324px";
+    }
+});
